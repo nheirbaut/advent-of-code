3ad4202aa635cd708fa1e91939b788faec4144e6
Niels Heirbaut <niels@opilo.nl>
2024-12-05 16:17:48 +0000

    Part 2 of day 5

--- 2024/05/solution_2024_05.py
+++ 2024/05/solution_2024_05.py
@@ -90,51 +90,97 @@ def get_updates_from_file(file_path: str) -> list[list[int]]:
     return get_updates_from_lines(update_lines)
 
 
-def get_valid_updates(
+def get_valid_and_invalid_updates(
     updates: list[list[int]],
     before_rules: dict[int, list[int]],
-) -> list[list[int]]:
-    """Get the valid updates.
+) -> tuple[list[list[int]], list[list[int]]]:
+    """Get the valid and invalid updates.
 
     Args:
         updates (list[list[int]]): The updates to validate
         before_rules (dict[int, list[int]]): The rules to validate for
 
     Returns:
-        list[list[int]]: The validated updates
+        tuple[list[list[int]], list[list[int]]]: The valid and invalid updates
     """
     valid_updates: list[list[int]] = []
+    invalid_updates: list[list[int]] = []
 
     for update in updates:
-        valid = True
-
-        for page_index, page_number in enumerate(update):
-            if page_number in before_rules:
-                must_be_before_pages = before_rules[page_number]
-
-                pages_preceding = update[:page_index]
-                if any(
-                    page_preceding in must_be_before_pages
-                    for page_preceding in pages_preceding
-                ):
-                    valid = False
-                    break
-
-            pages_following = update[page_index + 1 :]
-            for page_following in pages_following:
-                if page_following in before_rules:
-                    following_page_before_rule = before_rules[page_following]
-                    if page_number in following_page_before_rule:
-                        valid = False
-                        break
-
-            if not valid:
-                break
-
+        valid = is_valid_update(update, before_rules)
         if valid:
             valid_updates.append(update)
+        else:
+            invalid_updates.append(update)
 
-    return valid_updates
+    return valid_updates, invalid_updates
+
+
+def get_corrected_updates(
+    invalid_updates: list[list[int]], before_rules: dict[int, list[int]]
+) -> list[list[int]]:
+    """Correct the given updates.
+
+    Args:
+        invalid_updates (list[list[int]]): The updates to correct
+        before_rules (dict[int, list[int]]): The rule to validate an update
+
+    Returns:
+        list[list[int]]: The corrected updates
+    """
+    corrected_updates: list[list[int]] = []
+
+    for invalid_update in invalid_updates:
+        corrected_update: list[int] = []
+
+        for current_page in invalid_update:
+            index: list[int] = []
+            before_rules_for_page = before_rules.get(current_page, [])
+
+            for required_after in before_rules_for_page:
+                if required_after in corrected_update:
+                    index_of_page_required_after = corrected_update.index(required_after)
+                    index.append(index_of_page_required_after)
+
+            if index:
+                corrected_update.insert(min(index), current_page)
+            else:
+                corrected_update.append(current_page)
+
+        corrected_updates.append(corrected_update)
+
+    return corrected_updates
+
+
+def is_valid_update(update: list[int], before_rules: dict[int, list[int]]) -> bool:
+    """Determine if an update is valid.
+
+    Args:
+        update (list[int]): The update to validate
+        before_rules (dict[int, list[int]]): The rules to validate against
+
+    Returns:
+        bool: True if an update adheres to the rules
+    """
+    for page_index, page_number in enumerate(update):
+        if page_number in before_rules:
+            must_be_before_pages = before_rules[page_number]
+
+            pages_preceding = update[:page_index]
+            if any(
+                page_preceding in must_be_before_pages
+                for page_preceding in pages_preceding
+            ):
+                return False
+
+        pages_following = update[page_index + 1 :]
+        for page_following in pages_following:
+            if page_following in before_rules:
+                following_page_before_rule = before_rules[page_following]
+                if page_number in following_page_before_rule:
+                    return False
+
+    return True
 
 
 def main() -> None:
@@ -142,10 +188,17 @@ def main() -> None:
     before_rules = get_rules_from_file("rules.txt")
     updates = get_updates_from_file("input.txt")
 
-    valid_updates = get_valid_updates(updates, before_rules)
+    valid_updates, invalid_updates = get_valid_and_invalid_updates(
+        updates, before_rules
+    )
 
     sum1 = sum_of_all_middle_values(valid_updates)
-    print(f"The sum is: {sum1}")
+    print(f"The sum for part 1 is: {sum1}")
+
+    corrected_updates = get_corrected_updates(invalid_updates, before_rules)
+
+    sum2 = sum_of_all_middle_values(corrected_updates)
+    print(f"The sum for part 2 is: {sum2}")
 
 
 if __name__ == "__main__":
--- 2024/05/test_solution_2024_05.py
+++ 2024/05/test_solution_2024_05.py
@@ -1,7 +1,8 @@
 from solution_2024_05 import (
+    get_corrected_updates,
     get_rules_from_lines,
     get_updates_from_lines,
-    get_valid_updates,
+    get_valid_and_invalid_updates,
     sum_of_all_middle_values,
 )
 
@@ -89,5 +90,56 @@ def test_example_puzzle_input() -> None:
         [97, 61, 53, 29, 13],
         [75, 29, 13],
     ]
+    expected_invalid_updates = [
+        [75, 97, 47, 61, 53],
+        [61, 13, 29],
+        [97, 13, 75, 29, 47],
+    ]
     test_before_rules = get_rules_from_lines(test_rule_lines)
-    assert get_valid_updates(test_updates, test_before_rules) == expected_valid_updates
+    valid_updates, invalid_updates = get_valid_and_invalid_updates(
+        test_updates, test_before_rules
+    )
+    assert valid_updates == expected_valid_updates
+    assert invalid_updates == expected_invalid_updates
+
+
+def test_update_correction() -> None:
+    """Test correcting invalid updates."""
+    test_invalid_updates = [
+        [75, 97, 47, 61, 53],
+        [61, 13, 29],
+        [97, 13, 75, 29, 47],
+    ]
+    test_rule_lines = [
+        "47|53",
+        "97|13",
+        "97|61",
+        "97|47",
+        "75|29",
+        "61|13",
+        "75|53",
+        "29|13",
+        "97|29",
+        "53|29",
+        "61|53",
+        "97|53",
+        "61|29",
+        "47|13",
+        "75|47",
+        "97|75",
+        "47|61",
+        "75|61",
+        "47|29",
+        "75|13",
+        "53|13",
+    ]
+    expected_corrected_rules = [
+        [97, 75, 47, 61, 53],
+        [61, 29, 13],
+        [97, 75, 47, 29, 13],
+    ]
+    test_before_rules = get_rules_from_lines(test_rule_lines)
+    assert (
+        get_corrected_updates(test_invalid_updates, test_before_rules)
+        == expected_corrected_rules
+    )
